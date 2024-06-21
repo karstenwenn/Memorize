197fc204650fb04ba7b645857e9469facd148ddb
Karsten Wennerlund <kandw15@gmail.com>
2024-06-21 15:14:03 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Karsten Wennerlund on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
