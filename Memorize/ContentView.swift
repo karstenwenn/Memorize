b99161ce0e1186bc6b4ecb1c939c183be219cd66
Karsten Wennerlund <kandw15@gmail.com>
2024-06-21 15:56:28 -0500

    Toggleable is face up

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(faceUp: true)
+            CardView()
             CardView()
             CardView()
             CardView()
@@ -22,24 +22,24 @@ struct ContentView: View {
 }
 
 struct CardView: View{
-    var faceUp = false
-    
+    @State var isFaceUp = false
     
     var body: some View{
-        if faceUp{
-            ZStack(content: {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+        
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp{
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text ("👻").font(.largeTitle)
-                   })
+            }
+            else{
+                base.fill()
+            }
+        } .onTapGesture {
+            isFaceUp.toggle()
         }
-        else{
-            ZStack(content: {
-                RoundedRectangle(cornerRadius: 12)
-                   })
-        }
+               
         
     }
 }
